23f424ea9474de10d6d8a2b3206b5dcd5346266d
Saicharan346 <csai13323@gmail.com>
2025-11-26 21:42:53 +0530

    Added Output

--- firstpy.py
+++ firstpy.py
@@ -8,5 +8,14 @@ for j in range(n):
     if(list1[j]==key):
         print("Element Found!!!")
         break
-    
+'''Sample Output:
+   Enter how many elements in the list5
+   Enter the 1 element of the list1 
+   Enter the 2 element of the list2
+   Enter the 3 element of the list3
+   Enter the 4 element of the list4
+   Enter the 5 element of the list5
+   Enter the key to check in the list3
+   Element Found!!!
+   '''
 
