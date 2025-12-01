7fe3e3145469968d937342b7ae023a59b1b87c44
Saicharan346 <csai13323@gmail.com>
2025-12-01 11:33:53 +0530

    Commited

---
+++ pandas1.py
@@ -0,0 +1,7 @@
+import pandas as pd  # This line is essential!
+
+# Read the CSV file
+df = pd.read_csv("C:\\Users\\Kande_SaiCharanTej\\Desktop\\Python\\mypythonnotes\\smpledata.csv")
+
+# Print the DataFrame
+print(df)
