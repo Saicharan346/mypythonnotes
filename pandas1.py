4d4e9891a34ca86066eb1c496e50d0bdbb3badf4
Saicharan346 <csai13323@gmail.com>
2025-12-01 12:19:00 +0530

    Commited

--- pandas1.py
+++ pandas1.py
@@ -8,4 +8,18 @@ print(df)
 data={'name':['Sai','Charan','Tej'],'marks':[12,13,13]}
 bf=pd.DataFrame(data)
 print(bf)
-df.to_csv('output.csv',index=False)
+bf.to_csv('output.csv',index=False)
+'''
+head() if not entered any number in () default 5 first five rows
+tail() if not entered any number in () default 5 last five rows
+'''
+print(df.head(5))
+'''
+info() -method
+1-no of rows and columns
+2-coulmn name
+3-int64,float64 object
+4-non null count
+5-memory usage of the dataset
+'''
+print(df.info())
