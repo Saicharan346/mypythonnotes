ee95df0275c12e1bf1cbfbc03b033b800aab95b8
Saicharan346 <csai13323@gmail.com>
2025-12-01 12:01:42 +0530

    Commited

--- pandas1.py
+++ pandas1.py
@@ -1,7 +1,11 @@
 import pandas as pd  # This line is essential!
 
 # Read the CSV file
-df = pd.read_csv("C:\\Users\\Kande_SaiCharanTej\\Desktop\\Python\\mypythonnotes\\smpledata.csv")
-
+#df = pd.read_csv("smpledata.csv")
+df=pd.read_excel("smpledata.xlsx")
 # Print the DataFrame
 print(df)
+data={'name':['Sai','Charan','Tej'],'marks':[12,13,13]}
+bf=pd.DataFrame(data)
+print(bf)
+df.to_csv('output.csv')
